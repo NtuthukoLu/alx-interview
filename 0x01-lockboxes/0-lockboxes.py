976f255fd8bb9428c8693a473cc3d8378b79466c
Ntuthuko <ntuthukolu@gmail.com>
2023-12-08 04:42:43 +0200

    pycodestyle

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
+"""Solves lockboxes puzzle"""
+
 
 def canUnlockAll(boxes):
-    # Set to keep track of visited boxes
+    """ Set to keep track of visited boxes
+        and stack to perform DFS
+    """
     visited = set()
-    # Stack to perform DFS
-    stack = [0]  # Start with the first box (index 0)
+    stack = [0]
 
     while stack:
         current_box = stack.pop()
         visited.add(current_box)
 
-        # Add new keys to the stack
         for key in boxes[current_box]:
             if key not in visited and key < len(boxes):
                 stack.append(key)
 
-    # Check if all boxes have been visited
     return len(visited) == len(boxes)
-
