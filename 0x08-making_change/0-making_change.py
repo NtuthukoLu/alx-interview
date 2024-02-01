9543259de62ad918bce809b27c9b2815284cc36e
Ntuthuko <ntuthukolu@gmail.com>
2024-02-01 23:50:20 +0200

    task 0 change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+ This function initializes a dynamic programming table (dp)
+ to store the minimum number of coins needed for each total amount
+ from 0 to the target total.
+ """
+
+
+def makeChange(coins, total):
+    """
+     It iterates through each coin and updates the table accordingly
+    """
+    if total <= 0:
+        return 0
+    dp = [float('inf')] * (total + 1)
+
+    dp[0] = 0
+
+    for coin in coins:
+
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    if dp[total] == float('inf'):
+        return -1
+
+    return dp[total]
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,7 @@
+# PROJECT: Making Change
+
+AUTHOR: Ntuthuko Zimu
+
+## TASKS
+
+### 0. Change comes from within - `0-making_change.py`
